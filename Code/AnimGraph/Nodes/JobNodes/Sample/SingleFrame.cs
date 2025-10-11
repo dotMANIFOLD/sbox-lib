70f22c3b198b6725328535d574611066977d0f24
dotRYEN <work@dotryen.me>
2025-10-10 23:05:29 -0400

    animgraph: implement single frame node

--- Code/AnimGraph/Nodes/JobNodes/Sample/SingleFrame.cs
+++ Code/AnimGraph/Nodes/JobNodes/Sample/SingleFrame.cs
@@ -1,24 +1,51 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json.Serialization;
+using MANIFOLD.AnimGraph.Jobs;
+using Sandbox;
 
 namespace MANIFOLD.AnimGraph.Nodes {
+    /// <summary>
+    /// Sample a single frame of an animation.
+    /// </summary>
     [Category(JobCategories.SAMPLING)]
     [ExposeToAnimGraph]
     public class SingleFrame : JobNode {
-        public string Animation { get; set; }
+        public AnimationRef Clip { get; set; } = new();
         public int Frame { get; set; }
 
-        [JsonIgnore]
-        public override string DisplayName => "Single Frame";
-        [JsonIgnore]
+        [Hide, JsonIgnore]
+        public override string DisplayName => $"Single Frame: {Clip.Name} // {Frame}";
+        [Hide, JsonIgnore]
         public override Color AccentColor => JobCategories.SAMPLING_COLOR;
         
         public override IBaseAnimJob CreateJob(in JobCreationContext ctx) {
-            throw new System.NotImplementedException();
+            var job = new SampleJob(ID);
+            
+            // CLIP
+            switch (Clip.Mode) {
+                case ResourceRef.RefMode.Named: {
+                    job.Clip = ctx.resources.Animations.FirstOrDefault(x => x.Name == Clip.Name);
+                    break;
+                }
+                case ResourceRef.RefMode.Direct: {
+                    job.Clip = Clip.DirectReference;
+                    break;
+                }
+                default: {
+                    Log.Warning($"Unhandled ResourceRef mode: {Clip.Mode}");
+                    break;
+                }
+            }
+
+            job.PlaybackSpeed = 0;
+            job.Time = (1f / job.Clip.FrameRate) * Frame;
+            
+            return job;
         }
 
         public override IEnumerable<NodeRef> GetInputs() {
-            throw new System.NotImplementedException();
+            return [];
         }
     }
 }
