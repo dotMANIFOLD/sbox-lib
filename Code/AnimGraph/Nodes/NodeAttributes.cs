9dfc3269f72e0e224e2b6900baa7adaf91b71be5
dotRYEN <work@dotryen.me>
2025-10-12 01:21:41 -0400

    animgraph: add docs to node attributes

--- Code/AnimGraph/Nodes/NodeAttributes.cs
+++ Code/AnimGraph/Nodes/NodeAttributes.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 
 namespace MANIFOLD.AnimGraph {
+    /// <summary>
+    /// Exposes this class to the AnimGraph.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
     public class ExposeToAnimGraphAttribute : Attribute {
         public string Color { get; set; } = "#ececec";
     }
     
+    /// <summary>
+    /// Marks this property as an input. Only works for <see cref="NodeRef"/>.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class InputAttribute : Attribute { }
     
+    /// <summary>
+    /// Changes to properties with this attribute should modify node inputs.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class UpdatesInputsAttribute : Attribute { }
 }
