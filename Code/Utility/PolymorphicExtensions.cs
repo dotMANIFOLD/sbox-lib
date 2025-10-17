4e36804d394d9203c94d03d03f6f83ef1467abac
dotRYEN <work@dotryen.me>
2025-10-16 22:12:03 -0400

    utility: polymorphic handles prims

--- Code/Utility/PolymorphicExtensions.cs
+++ Code/Utility/PolymorphicExtensions.cs
@@ -6,14 +6,24 @@ using Sandbox;
 
 namespace MANIFOLD.Utility {
     public static class PolymorphicExtensions {
+        public const string PRIMITIVE_FIELD = "__prim";
+        public const string VALUE_FIELD = "__value";
         public const string TYPE_FIELD = "__type";
         public const string TYPE_ELEMENT_FIELD = "__elements";
         public const string NULL_KEY = "__null";
 
         public static JsonNode ToPolymorphic<T>(this T obj) {
-            JsonNode node = Json.ToNode(obj);
             var type = obj.GetType();
             var typeDef = TypeLibrary.GetType(type);
+
+            JsonNode node = Json.ToNode(obj);
+            if (IsPrimitive(type)) {
+                JsonObject jsonObj = new JsonObject();
+                jsonObj[PRIMITIVE_FIELD] = Json.ToNode(true);
+                jsonObj[VALUE_FIELD] = node;
+                node = jsonObj;
+            }
+            
             if (typeDef.IsGenericType) {
                 node[TYPE_FIELD] = Json.ToNode(typeDef.TargetType, typeof(Type));
                 node[TYPE_ELEMENT_FIELD] = Json.ToNode(typeDef.GenericArguments);
@@ -27,6 +37,12 @@ namespace MANIFOLD.Utility {
         public static T FromPolymorphic<T>(this JsonNode node) {
             var type = Json.FromNode<Type>(node[TYPE_FIELD]);
             var typeDef = TypeLibrary.GetType(type);
+            
+            var primNode = node[PRIMITIVE_FIELD];
+            if (primNode != null && primNode.GetValue<bool>()) {
+                return (T)Json.FromNode(node[VALUE_FIELD], type);
+            }
+            
             if (typeDef.IsGenericType) {
                 var elements = Json.FromNode<Type[]>(node[TYPE_ELEMENT_FIELD]);
                 type = typeDef.MakeGenericType(elements);
@@ -77,6 +93,28 @@ namespace MANIFOLD.Utility {
                 onCreate?.Invoke(key, value);
             }
             return dict;
+        }
+
+        public static bool IsPrimitive(Type type) {
+            if (type == typeof(bool)) return true;
+            if (type == typeof(byte)) return true;
+            if (type == typeof(sbyte)) return true;
+            if (type == typeof(short)) return true;
+            if (type == typeof(ushort)) return true;
+            if (type == typeof(int)) return true;
+            if (type == typeof(uint)) return true;
+            if (type == typeof(long)) return true;
+            if (type == typeof(ulong)) return true;
+            if (type == typeof(float)) return true;
+            if (type == typeof(double)) return true;
+            if (type == typeof(decimal)) return true;
+            if (type == typeof(string)) return true;
+            if (type == typeof(Vector2)) return true;
+            if (type == typeof(Vector3)) return true;
+            if (type == typeof(Vector4)) return true;
+            if (type == typeof(Rotation)) return true;
+            if (type == typeof(Angles)) return true;
+            return false;
         }
     }
 }
